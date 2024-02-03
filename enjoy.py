55ba515afcf6785cb4fcea56394c46e81a4f6665
ger01d <79108045+ger01d@users.noreply.github.com>
2024-02-03 18:14:41 +0100

    Update for opencat-esp32
    New environment and model is now fitted to train a walking controller for the eps32 version of opencat-esp32. 
    Updates include:
    - Latencies in simulation environment fitted to esp32 board
    - URDF-model weight and geometry updates.

--- enjoy.py
+++ enjoy.py
@@ -1,22 +1,27 @@
 import time
 import pybullet as p
-
 from stable_baselines3 import PPO, SAC
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.env_util import make_vec_env
-from opencat_gym_env import OpenCatGymEnv
+from opencat_gym_esp32_env import OpenCatGymEnv
 
 # Create OpenCatGym environment from class
-env = OpenCatGymEnv()
+parallel_env = 1
+env = make_vec_env(OpenCatGymEnv, n_envs=parallel_env)
 
-model = PPO.load("trained/ppo_reference_no_penalty")
+model = PPO.load("trained/opencat_gym_release_local_2e6")
+
 obs = env.reset()
+sum_reward = 0
 
-while True:    
+for i in range(500):    
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    
+    sum_reward += reward
     env.render(mode="human")
+ 
     
     if done:
+        print("Reward", sum_reward[0])
+        sum_reward = 0
         obs = env.reset()
